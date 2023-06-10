736d91a52528820f076ce1937010abeaad2fd47b
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-06-10 06:18:08 +0600

    handle login add

--- src/pages/Dashboard/Sidebar/StuNavlistPc.jsx
+++ src/pages/Dashboard/Sidebar/StuNavlistPc.jsx
@@ -1,4 +1,3 @@
-
 import DashCustomNavlink from "./DashCustomNavlink";
 import { Badge } from "@mui/material";
 import { AiOutlineHeart } from "react-icons/ai";
@@ -6,15 +5,59 @@ import { Link } from "react-router-dom";
 import JoinInnerIcon from "@mui/icons-material/JoinInner";
 import PaidIcon from "@mui/icons-material/Paid";
 import DashCusNavMobile from "./DashCusNavMobile";
+import useAdmin from "../../../hooks/useAdmin";
+import useInstructor from "../../../hooks/useInstructor";
 
 export const StuNavlistPc = () => {
-    //TODO : GET STUDENT FORM HOOK
-  const student = false;
-  
+  //TODO : GET STUDENT FORM HOOK
+  const [isAdmin] = useAdmin();
+  const [isInstructor] = useInstructor();
+
   return (
     <div>
-      {student && (
-        <DashCustomNavlink
+      
+      { !isAdmin && !isInstructor && <DashCustomNavlink
+            to="/dashboard/bookedClass"
+            title="Booked Class"
+            icon={
+              <Link to="/dashboard/favorites">
+                <Badge badgeContent={4} color="primary">
+                  <li className="text-2xl hover:text-[#00ADEF] hover:transition hover:duration-500 cursor-pointer">
+                    <AiOutlineHeart></AiOutlineHeart>
+                  </li>
+                </Badge>
+              </Link>
+            }
+          ></DashCustomNavlink>}
+
+      {!isAdmin && !isInstructor && (
+          <DashCustomNavlink
+            to="/dashboard/enrolledClass"
+            title="Enrolled Class"
+            icon={<JoinInnerIcon></JoinInnerIcon>}
+          ></DashCustomNavlink>
+        )}
+
+      {!isAdmin && !isInstructor && (
+          <DashCustomNavlink
+            to="/dashboard/paymentHistory"
+            title="Payment History"
+            icon={<PaidIcon></PaidIcon>}
+          ></DashCustomNavlink>
+        )}
+    </div>
+  );
+};
+
+export const StuNavlistMobile = ({ handleDrawerToggle }) => {
+  //TODO : GET STUDENT FORM HOOK
+  const [isAdmin] = useAdmin();
+  const [isInstructor] = useInstructor();
+
+  return (
+    <div>
+      {!isAdmin && !isInstructor &&  (
+        <DashCusNavMobile
           to="/dashboard/bookedClass"
           title="Booked Class"
           icon={
@@ -26,51 +69,27 @@ export const StuNavlistPc = () => {
               </Badge>
             </Link>
           }
-        ></DashCustomNavlink>
+          handleDrawerToggle={handleDrawerToggle}
+        ></DashCusNavMobile>
       )}
 
-      {student && (
-        <DashCustomNavlink
+      {!isAdmin && !isInstructor &&  (
+        <DashCusNavMobile
           to="/dashboard/enrolledClass"
           title="Enrolled Class"
           icon={<JoinInnerIcon></JoinInnerIcon>}
-        ></DashCustomNavlink>
+          handleDrawerToggle={handleDrawerToggle}
+        ></DashCusNavMobile>
       )}
 
-      {student && (
-        <DashCustomNavlink
+      {!isAdmin && !isInstructor &&  (
+        <DashCusNavMobile
           to="/dashboard/paymentHistory"
           title="Payment History"
           icon={<PaidIcon></PaidIcon>}
-        ></DashCustomNavlink>
+          handleDrawerToggle={handleDrawerToggle}
+        ></DashCusNavMobile>
       )}
     </div>
   );
 };
-
-export const StuNavlistMobile = ({handleDrawerToggle}) => {
-    //TODO : GET STUDENT FORM HOOK
-  const student = false;
-  
-  return (
-    <div>
-      {student && (
-        <DashCusNavMobile to="/dashboard/bookedClass" title="Booked Class" icon={<Link to='/dashboard/favorites'>
-        <Badge badgeContent={4} color="primary">
-          <li className="text-2xl hover:text-[#00ADEF] hover:transition hover:duration-500 cursor-pointer">
-            <AiOutlineHeart></AiOutlineHeart>
-          </li>
-        </Badge></Link>} handleDrawerToggle={handleDrawerToggle}></DashCusNavMobile>
-      )}
-
-      {student && (
-        <DashCusNavMobile to="/dashboard/enrolledClass" title="Enrolled Class" icon={<JoinInnerIcon></JoinInnerIcon>} handleDrawerToggle={handleDrawerToggle}></DashCusNavMobile>
-      )}
-
-      {student && (
-        <DashCusNavMobile to="/dashboard/paymentHistory" title="Payment History" icon={<PaidIcon></PaidIcon>} handleDrawerToggle={handleDrawerToggle}></DashCusNavMobile>
-      )}
-    </div>
-  );
-};
-
